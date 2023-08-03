78a005bbee915062f64abe8964bf268e3d2c32b6
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-02 19:17:48 -0700

    [add] use https

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import http from 'http';
+import https from 'https';
+import * as fs from 'fs';
 import { app } from './4.F&D/app';
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+const options = {
+  key: fs.readFileSync('credentials/key.pem'),
+  cert: fs.readFileSync('credentials/cert.pem'),
+};
 
-const server: http.Server<
-  typeof http.IncomingMessage,
-  typeof http.ServerResponse
-> = http.createServer(app);
+const server: https.Server = https.createServer(options, app);
 
 async function startServer() {
   server.listen(PORT, () => console.log(`READY IN PORT [${PORT}]`));
