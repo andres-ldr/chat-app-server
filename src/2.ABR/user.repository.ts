447c4b6c876b3a740c2e3e70362d656cc4fc019c
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-04 19:23:37 -0700

    [edit] respond with userDTO

--- src/2.ABR/user.repository.ts
+++ src/2.ABR/user.repository.ts
@@ -1,7 +1,9 @@
+import { UserDTO } from '../1.EBR/UserDTO';
 import { UserEntity } from '../1.EBR/user.entity';
 
 export default interface UserRepository {
-  getUserById(uuid: string): Promise<UserEntity | null>;
-  postNewUser(user: UserEntity): Promise<UserEntity | null>;
-  fetchAllUsers(): Promise<UserEntity[] | []>;
+  getUserById(uuid: string): Promise<UserDTO>;
+  postNewUser(user: UserEntity): Promise<UserDTO>;
+  fetchAllUsers(): Promise<UserDTO[]>;
+  postNewContact(userId: string, email: string): Promise<UserDTO>;
 }
