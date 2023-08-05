478a981c5a509ddc0b4442f7b05c7c6f212e752a
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-04 19:26:09 -0700

    [add] helmet

--- src/4.F&D/app.ts
+++ src/4.F&D/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import { json } from 'body-parser';
 import cors from 'cors';
 import { api } from './api';
+import helmet from 'helmet';
 
 export const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(json());
 
