fb745c892021bd28904b6879f6791f9e0c85a584
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-04 19:26:52 -0700

    [add] error handling

--- src/4.F&D/repository/mockRepository.ts
+++ src/4.F&D/repository/mockRepository.ts
@@ -1,20 +1,57 @@
-import { UserEntity } from '../../1.EBR/user.entity';
+import { UserDTO } from '../../1.EBR/UserDTO';
 import UserRepository from '../../2.ABR/user.repository';
 
-const mockUsers: UserEntity[] = [];
+const mockUsers: UserDTO[] = [];
 
 export default class MockUserRespository implements UserRepository {
   constructor() {}
-
-  async getUserById(uuid: string): Promise<UserEntity | null> {
-    throw new Error('Method not implemented.');
+  async postNewContact(userId: string, email: string): Promise<UserDTO> {
+    let contact: UserDTO | undefined;
+    let user: UserDTO | undefined;
+    try {
+      contact = await mockUsers.find((e) => e.email === email);
+      if (contact === undefined) {
+        throw new Error();
+      }
+    } catch (err) {
+      throw new Error();
+    }
+    user = await this.getUserById(userId);
+    try {
+      user.contacts.push(contact.uid);
+    } catch (err) {
+      throw new Error();
+    }
+    return user;
   }
-  async postNewUser(newUser: UserEntity): Promise<UserEntity | null> {
-    console.log(mockUsers);
-    mockUsers.push(newUser);
+
+  async getUserById(id: string): Promise<UserDTO> {
+    let user: UserDTO | undefined;
+    try {
+      user = await mockUsers.find((e) => e.uid === id);
+      if (user === undefined || !user) {
+        throw Error();
+      }
+    } catch (err) {
+      throw new Error();
+    }
+    return user;
+  }
+  async postNewUser(newUser: UserDTO): Promise<UserDTO> {
+    try {
+      mockUsers.push(newUser);
+    } catch (err) {
+      throw new Error();
+    }
     return newUser;
   }
-  async fetchAllUsers(): Promise<UserEntity[] | []> {
-    return mockUsers;
+  async fetchAllUsers(): Promise<UserDTO[]> {
+    let allUsers: UserDTO[];
+    try {
+      allUsers = Array.from(mockUsers);
+    } catch (err) {
+      throw new Error();
+    }
+    return allUsers;
   }
 }
