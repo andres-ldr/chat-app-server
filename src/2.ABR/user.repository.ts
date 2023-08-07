daf93054b44e53bbc9578cec268881f2d7ca338d
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-07 09:42:26 -0700

    [add] temporary types

--- src/2.ABR/user.repository.ts
+++ src/2.ABR/user.repository.ts
@@ -1,12 +1,47 @@
+import { QueryResult } from 'pg';
 import ChatDTO from '../1.EBR/ChatDTO';
 import MsgDTO from '../1.EBR/MsgDTO';
 import { UserDTO } from '../1.EBR/UserDTO';
 
 export default interface UserRepository {
-  getUserById(uuid: string): Promise<UserDTO>;
-  postNewUser(user: UserDTO): Promise<UserDTO>;
-  fetchAllUsers(): Promise<UserDTO[]>;
-  postNewContact(userId: string, email: string): Promise<UserDTO>;
+  getUserById(uuid: string): Promise<{
+    uid: string;
+    name: string;
+    last_name: string;
+    email: string;
+    profile_image: string;
+    creation_date: Date;
+  } | null>;
+  postNewUser(user: UserDTO): Promise<{
+    uid: string;
+    name: string;
+    last_name: string;
+    email: string;
+    profile_image: string;
+    creation_date: Date;
+  } | null>;
+  fetchAllUsers(): Promise<
+    | {
+        uid: string;
+        name: string;
+        last_name: string;
+        email: string;
+        profile_image: string;
+        creation_date: Date;
+      }[]
+    | null
+  >;
+  postNewContact(
+    userId: string,
+    email: string
+  ): Promise<{
+    uid: string;
+    name: string;
+    last_name: string;
+    email: string;
+    profile_image: string;
+    creation_date: Date;
+  } | null>;
   postNewChat(chat: ChatDTO): Promise<ChatDTO>;
   postNewMsg(msg: MsgDTO, chatId: string): Promise<MsgDTO>;
   fetchChatMsgs(chatId: string): Promise<MsgDTO[]>;
