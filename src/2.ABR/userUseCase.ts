e0e08be98661677d2eb54a7ee56e577340407e60
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-07 09:42:09 -0700

    [add] temporary types

--- src/2.ABR/userUseCase.ts
+++ src/2.ABR/userUseCase.ts
@@ -1,3 +1,4 @@
+import { QueryResult } from 'pg';
 import ChatDTO from '../1.EBR/ChatDTO';
 import MsgDTO from '../1.EBR/MsgDTO';
 import { UserDTO, userSimpleData } from '../1.EBR/UserDTO';
@@ -11,25 +12,62 @@ export default class UserUsesCases {
     name,
     last_name,
     email,
+    password,
     profile_image,
   }: userSimpleData) {
-    const userValue = new UserDTO({ name, last_name, email, profile_image });
-    const userCreated = await this.userRepository.postNewUser(userValue);
+    const userValue = new UserDTO({
+      name,
+      last_name,
+      email,
+      password,
+      profile_image,
+    });
+    const userCreated: {
+      uid: string;
+      name: string;
+      last_name: string;
+      email: string;
+      profile_image: string;
+      creation_date: Date;
+    } | null = await this.userRepository.postNewUser(userValue);
     return userCreated;
   }
   public async findUserById(id: string) {
-    const resp: UserDTO = await this.userRepository.getUserById(id);
+    const resp: {
+      uid: string;
+      name: string;
+      last_name: string;
+      email: string;
+      profile_image: string;
+      creation_date: Date;
+    } | null = await this.userRepository.getUserById(id);
     return resp;
   }
 
   public async getAllUsers() {
-    const allUsers: UserDTO[] = await this.userRepository.fetchAllUsers();
+    const allUsers:
+      | {
+          uid: string;
+          name: string;
+          last_name: string;
+          email: string;
+          profile_image: string;
+          creation_date: Date;
+        }[]
+      | null = await this.userRepository.fetchAllUsers();
     return allUsers;
   }
 
   public async addNewContact(uid: string, email: string) {
-    const message = this.userRepository.postNewContact(uid, email);
-    return message;
+    const response: {
+      uid: string;
+      name: string;
+      last_name: string;
+      email: string;
+      profile_image: string;
+      creation_date: Date;
+    } | null = await this.userRepository.postNewContact(uid, email);
+    return response;
   }
 
   public async createNewChat(contactId: string, userId: string) {
