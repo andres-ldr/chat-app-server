788d8605821949e511ca58fdb6efc963df7f8d49
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-07 10:13:54 -0700

    [add] password attr

--- src/1.EBR/user.entity.ts
+++ src/1.EBR/user.entity.ts
@@ -1,9 +1,8 @@
-import { ChatEntity } from './chat.entity';
-
 export interface UserEntity {
   name: string;
   last_name: string;
   email: string;
+  password: string;
   contacts: [];
   profile_image: string;
   chats: [];
