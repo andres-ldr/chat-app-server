457fa6fd3e38449e184eb92fb56a9989ecf1b915
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-10 13:42:12 -0700

    [edit] improve the params and returned types

--- src/2.ABR/user.repository.ts
+++ src/2.ABR/user.repository.ts
@@ -1,48 +1,27 @@
 import { QueryResult } from 'pg';
 import ChatDTO from '../1.EBR/ChatDTO';
 import MsgDTO from '../1.EBR/MsgDTO';
-import { UserDTO } from '../1.EBR/UserDTO';
+import { UserEntity } from '../1.EBR/user.entity';
+import { userResponseType } from '../1.EBR/UserDTO';
+import { ChatEntity } from '../1.EBR/chat.entity';
+import { MsgEntity } from '../1.EBR/msg.entity';
 
 export default interface UserRepository {
-  getUserById(uuid: string): Promise<{
-    uid: string;
-    name: string;
-    last_name: string;
-    email: string;
-    profile_image: string;
-    creation_date: Date;
-  } | null>;
-  postNewUser(user: UserDTO): Promise<{
-    uid: string;
-    name: string;
-    last_name: string;
-    email: string;
-    profile_image: string;
-    creation_date: Date;
-  } | null>;
-  fetchAllUsers(): Promise<
-    | {
-        uid: string;
-        name: string;
-        last_name: string;
-        email: string;
-        profile_image: string;
-        creation_date: Date;
-      }[]
-    | null
-  >;
+  getUserById(uuid: string): Promise<UserEntity | null>;
+  postNewUser(user: UserEntity): Promise<userResponseType>;
+  fetchAllChats(userId: string): Promise<ChatEntity[] | []>;
+  fetchAllContacts(uid: string): Promise<{}[]>;
   postNewContact(
     userId: string,
+    alias: string,
     email: string
-  ): Promise<{
-    uid: string;
-    name: string;
-    last_name: string;
-    email: string;
-    profile_image: string;
-    creation_date: Date;
-  } | null>;
-  postNewChat(chat: ChatDTO): Promise<ChatDTO>;
-  postNewMsg(msg: MsgDTO, chatId: string): Promise<MsgDTO>;
-  fetchChatMsgs(chatId: string): Promise<MsgDTO[]>;
+  ): Promise<{ email: string; authorId: string; alias: string } | null>;
+  postNewChat(alias: string, members: []): Promise<ChatEntity | null>;
+  postNewMsg(
+    chatId: string,
+    content: string,
+    type: string,
+    sender: string
+  ): Promise<MsgEntity | null>;
+  fetchChatMsgs(uid: string, chatId: string): Promise<MsgEntity[] | []>;
 }
