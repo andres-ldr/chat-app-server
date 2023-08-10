0f11341af0a43cc75cc91b52562512eb2dd21517
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-10 13:42:47 -0700

    [edit] delete uid from params

--- src/3.IA/routes/users.router.ts
+++ src/3.IA/routes/users.router.ts
@@ -1,20 +1,21 @@
 import { Router } from 'express';
 import UserUsesCases from '../../2.ABR/userUseCase';
 import UserController from '../controller/users.controller';
-import MockUserRespository from '../../4.F&D/repository/mockRepository';
+import PostgresRepository from '../../4.F&D/repository/postgresRepository';
 
 export const usersRouter = Router();
 
-const mockUserRepository = new MockUserRespository();
+const postgresRepository = new PostgresRepository();
 
-const userUseCases = new UserUsesCases(mockUserRepository);
+const userUseCases = new UserUsesCases(postgresRepository);
 
 const userController = new UserController(userUseCases);
 
 usersRouter.post('/', userController.postNewUser);
-usersRouter.get('/', userController.fetchAllUsers);
-usersRouter.get('/:uid', userController.fetchUserById);
-usersRouter.post('/:uid/new_contact', userController.postNewContact);
-usersRouter.post('/:uid/new_chat', userController.postNewChat);
-usersRouter.post('/:uid/send_msg', userController.postNewMsg);
-usersRouter.get('/:uid/chat', userController.fetchChatMsgs);
+usersRouter.get('/chats', userController.fetchChats);
+usersRouter.get('/contacts', userController.fetchAllContacts);
+// usersRouter.get('/:uid', userController.fetchUserById);
+usersRouter.post('/new_contact', userController.postNewContact);
+usersRouter.post('/new_chat', userController.postNewChat);
+usersRouter.post('/send_msg', userController.postNewMsg);
+usersRouter.get('/chat', userController.fetchChatMsgs);
