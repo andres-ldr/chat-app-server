c0f27eb6a26c6ed2bd783ddb0795282a85c6ff45
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-11 12:58:17 -0700

    [add] error handler middleware

--- src/4.F&D/app.ts
+++ src/4.F&D/app.ts
@@ -3,6 +3,7 @@ import { json } from 'body-parser';
 import cors from 'cors';
 import { api } from './api';
 import helmet from 'helmet';
+import ErrorHandler from './middleware/ErrorHandler';
 
 export const app = express();
 
@@ -11,3 +12,5 @@ app.use(cors());
 app.use(json());
 
 app.use('/v1', api);
+
+app.use(ErrorHandler);
