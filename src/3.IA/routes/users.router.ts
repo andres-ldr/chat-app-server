aa96922f09ef5ff927013f9b3f86b5734a416865
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-14 11:30:13 -0700

    [add] auth system for login and logout

--- src/3.IA/routes/users.router.ts
+++ src/3.IA/routes/users.router.ts
@@ -2,16 +2,21 @@ import { Router } from 'express';
 import UserUsesCases from '../../2.ABR/userUseCase';
 import UserController from '../controller/users.controller';
 import PostgresRepository from '../../4.F&D/repository/postgresRepository';
+import JwtRepository from '../../4.F&D/repository/JWTRepository';
 
 export const usersRouter = Router();
 
 const postgresRepository = new PostgresRepository();
+const jwtRepository = new JwtRepository(`${process.env.JWT_KEY}`, '1h');
 
-const userUseCases = new UserUsesCases(postgresRepository);
+const userUseCases = new UserUsesCases(postgresRepository, jwtRepository);
 
 const userController = new UserController(userUseCases);
 
 usersRouter.post('/', userController.postNewUser);
+usersRouter.post('/login', userController.postLogIn);
+usersRouter.use(userController.checkAuth);
+usersRouter.post('/logout', userController.postLogOut);
 usersRouter.get('/chats', userController.fetchChats);
 usersRouter.get('/contacts', userController.fetchAllContacts);
 // usersRouter.get('/:uid', userController.fetchUserById);
