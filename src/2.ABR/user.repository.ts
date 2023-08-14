c3ffa90cc378ee49018cb196333119bd32027b16
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-14 11:26:39 -0700

    [edit] returned types

--- src/2.ABR/user.repository.ts
+++ src/2.ABR/user.repository.ts
@@ -2,27 +2,33 @@ import { UserEntity } from '../1.EBR/user.entity';
 import { userResponseType } from '../1.EBR/Types';
 import { ChatEntity } from '../1.EBR/chat.entity';
 import { MsgEntity } from '../1.EBR/msg.entity';
+import ContactEntity from '../1.EBR/Contact.entity';
 
 export default interface UserRepository {
   getUserById(uuid: string): Promise<UserEntity | null>;
-  postNewUser(user: UserEntity): Promise<userResponseType>;
+  postNewUser(user: UserEntity): Promise<UserEntity>;
   fetchAllChats(userId: string): Promise<ChatEntity[] | []>;
   fetchAllContacts(uid: string): Promise<{}[]>;
   postNewContact(
     userId: string,
     alias: string,
     email: string
-  ): Promise<{ email: string; authorId: string; alias: string } | null>;
-  postNewChat(alias: string, members: []): Promise<ChatEntity | null>;
+  ): Promise<ContactEntity>;
+  postNewChat(
+    alias: string | null,
+    members: [],
+    adminId: { uid: string }[]
+  ): Promise<ChatEntity>;
   postNewMsg(
     chatId: string,
     content: string,
     type: string,
     sender: string
-  ): Promise<MsgEntity | null>;
+  ): Promise<MsgEntity>;
   fetchChatMsgs(uid: string, chatId: string): Promise<MsgEntity[] | []>;
-  userExists(uid: string): Promise<userResponseType>;
-  emailExists(email: string): Promise<userResponseType>;
+  userExists(uid: string): Promise<UserEntity>;
+  emailExists(email: string): Promise<UserEntity | null>;
   contactExists(authorId: string, email: string): Promise<{} | null>;
   lookUpForExistingChat(alias: string, participants: []): Promise<void>;
+  authenticateUser(email: string, password: string): Promise<UserEntity>;
 }
