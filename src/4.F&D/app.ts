747ec5f1bc10e4ff5a43b125afe1cc07f6cbdd4f
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-14 11:31:46 -0700

    [add] cookie parser

--- src/4.F&D/app.ts
+++ src/4.F&D/app.ts
@@ -4,12 +4,14 @@ import cors from 'cors';
 import { api } from './api';
 import helmet from 'helmet';
 import ErrorHandler from './middleware/ErrorHandler';
+import cookieParser from 'cookie-parser';
 
 export const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(json());
+app.use(cookieParser());
 
 app.use('/v1', api);
 
