ab43800ac7954a0beb4112dbbac4f5051278ae84
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-18 10:01:27 -0700

    [add] migration

---
+++ prisma/migrations/20230818170034_init/migration.sql
@@ -0,0 +1,104 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "uid" UUID NOT NULL,
+    "name" VARCHAR(30) NOT NULL,
+    "lastName" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "profileImage" VARCHAR(50) NOT NULL,
+    "creationDate" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("uid")
+);
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "contactId" UUID NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "authorId" UUID NOT NULL,
+    "alias" VARCHAR(20) NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("contactId")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "cid" UUID NOT NULL,
+    "alias" VARCHAR(30),
+    "creationDate" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("cid")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "mid" UUID NOT NULL,
+    "chatId" UUID NOT NULL,
+    "content" TEXT NOT NULL,
+    "type" VARCHAR(20) NOT NULL,
+    "creationDate" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "senderId" UUID NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("mid")
+);
+
+-- CreateTable
+CREATE TABLE "_members" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_admins" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_members_AB_unique" ON "_members"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_members_B_index" ON "_members"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_admins_AB_unique" ON "_admins"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_admins_B_index" ON "_admins"("B");
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("uid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("cid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("uid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_members" ADD CONSTRAINT "_members_A_fkey" FOREIGN KEY ("A") REFERENCES "Chat"("cid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_members" ADD CONSTRAINT "_members_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_admins" ADD CONSTRAINT "_admins_A_fkey" FOREIGN KEY ("A") REFERENCES "Chat"("cid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_admins" ADD CONSTRAINT "_admins_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
