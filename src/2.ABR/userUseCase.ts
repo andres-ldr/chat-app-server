fd96f71aa3dc1c0e7f53dc00c89ec9697a1c3422
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-02 17:19:41 -0700

    [add] improve chat usecases

--- src/2.ABR/userUseCase.ts
+++ src/2.ABR/userUseCase.ts
@@ -1,33 +1,51 @@
 import UserRepository from './user.repository';
 import { UserEntity } from '../1.EBR/user.entity';
-import BaseError from '../Utils/BaseError';
 
 export default class UserUsesCases {
   constructor(private readonly userRepository: UserRepository) {}
 
   public async addNewUser(newUser: UserEntity) {
-    let userAuthLimited: {
-      uid: string;
-      name: string;
-      email: string;
-      profileImage: string | null;
-    };
-
-    const { uid, name, email, profileImage } =
-      await this.userRepository.postNewUser(newUser);
-
-    userAuthLimited = {
-      uid,
-      name,
-      email,
-      profileImage,
-    };
-
-    return userAuthLimited;
+    return await this.userRepository.postNewUser(newUser);
   }
 
-  public async getAllChats(userId: string) {
-    return await this.userRepository.fetchAllChats(userId);
+  public async getAllChats(uid: string) {
+    const chats = await this.userRepository.fetchAllChats(uid);
+
+    const newChats: { cid: string; alias: string; chatImage: string }[] =
+      await Promise.all(
+        chats.map(async ({ cid, alias, chatImage }) => {
+          if (!alias) {
+            const { email, profileImage } =
+              await this.userRepository.findTheOtherMemberOfAChat(cid, uid);
+            const contact = await this.userRepository.findUsersContactByEmail(
+              uid,
+              email
+            );
+
+            if (contact) {
+              return {
+                cid,
+                alias: contact.alias,
+                chatImage: contact.profileImage,
+              };
+            } else {
+              return {
+                cid,
+                alias: email,
+                chatImage: profileImage,
+              };
+            }
+          } else {
+            return {
+              cid,
+              alias,
+              chatImage,
+            };
+          }
+        })
+      );
+
+    return newChats;
   }
 
   public async getAllContacts(uid: string) {
@@ -38,30 +56,42 @@ export default class UserUsesCases {
     return await this.userRepository.postNewContact(authorId, alias, email);
   }
 
-  public async createNewChat(
+  public async getChatByMembers(
     alias: string | null,
     members: [],
-    creatorId: string
+    uid: string,
+    adminId: {
+      uid: string;
+    }[],
+    chatImage: string | null
   ) {
-    let num_members: number = members.length;
-    let listOfAdmins = [];
+    const existingChat = await this.userRepository.findChatByMembers(
+      alias,
+      members
+    );
 
-    if (num_members < 2) {
-      throw new BaseError('A chat must have at least 2 members', 400);
+    if (existingChat) {
+      return await this.getChatById(uid, existingChat.cid);
+    } else {
+      const newChat = await this.createChat(alias, members, adminId, chatImage);
+      return await this.getChatById(uid, newChat.cid);
     }
+  }
 
-    if (num_members === 2 && alias !== undefined && alias !== null) {
-      alias = null;
-      //throw new BaseError('A normal chat does not require an alias', 400);
-    }
-    if (num_members > 2) {
-      if (alias === undefined || alias === null) {
-        throw new BaseError('A group must have an alias', 400);
-      }
-      listOfAdmins.push({ uid: creatorId });
-    }
-
-    return await this.userRepository.postNewChat(alias, members, listOfAdmins);
+  public async createChat(
+    alias: string | null,
+    members: [],
+    adminId: {
+      uid: string;
+    }[],
+    chatImage: string | null
+  ) {
+    return await this.userRepository.postNewChat(
+      alias,
+      members,
+      adminId,
+      chatImage
+    );
   }
 
   public async sendMsg(
@@ -78,7 +108,27 @@ export default class UserUsesCases {
     );
   }
 
-  public async getAllChatMsgs(uid: string, chatId: string) {
-    return this.userRepository.fetchChatMsgs(uid, chatId);
+  public async getChatById(uid: string, chatId: string) {
+    const { cid, alias, chatImage } = await this.userRepository.fetchChatById(
+      uid,
+      chatId
+    );
+    const chat = await this.userRepository.fetchChatMsgs(chatId);
+    const members = await this.userRepository.fetchChatMembers(chatId);
+
+    if (members.length > 2) {
+      return { cid, alias, chatImage, chat };
+    } else {
+      const { email, profileImage } = members.find(
+        (member) => member.uid !== uid
+      ) as UserEntity;
+
+      const contact = await this.userRepository.isAnUserContact(uid, email);
+      if (contact) {
+        return { cid, alias: contact.alias, chatImage: profileImage, chat };
+      } else {
+        return { cid, alias: email, chatImage: profileImage, chat };
+      }
+    }
   }
 }
