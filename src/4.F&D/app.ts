8b6e5e8da6e55fb8cb7e692b0796bfd4baf09cbc
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-02 17:15:22 -0700

    [edit] set for multer

--- src/4.F&D/app.ts
+++ src/4.F&D/app.ts
@@ -1,22 +1,28 @@
-import express from 'express';
-import { json } from 'body-parser';
-import cors from 'cors';
-import { api } from './api';
-import helmet from 'helmet';
 import ErrorHandler from './middleware/ErrorHandler';
+import { json } from 'body-parser';
+import express from 'express';
+import helmet from 'helmet';
+import { api } from './api';
+import cors from 'cors';
+import path from 'path';
 
 export const app = express();
 
-app.use(helmet());
-app.use(json({ limit: '200mb' }));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+  })
+);
+app.use(json({ limit: 10485760 }));
 
+// test config for express-session
 app.use(
   cors({
     origin: 'http://localhost:5173',
     credentials: true,
   })
 );
-
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use('/v1', api);
 
 app.use(ErrorHandler);
