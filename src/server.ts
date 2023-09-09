b8df5821f7b83762b8daf4701917dc39644e394e
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-09 13:32:32 -0700

    [wip] real time messages

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,35 @@
-import http from 'http';
-import * as fs from 'fs';
+import { Server as WebSocketServer } from 'socket.io';
 import { app } from './4.F&D/app';
 require('dotenv').config();
-
-const PORT = process.env.PORT;
-const options = {
-  key: fs.readFileSync('credentials/key.pem'),
-  cert: fs.readFileSync('credentials/cert.pem'),
-};
-
+import http from 'http';
+import { userUseCases } from './3.IA/routes/users.router';
 declare module 'express-session' {
   interface SessionData {
     passport: { user: string };
   }
 }
+
+const PORT = process.env.PORT;
+
 const server: http.Server = http.createServer(app);
+export const io = new WebSocketServer(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+  },
+});
 
 async function startServer() {
+  io.on('connection', (socket) => {
+    socket.on('CLIENT:JOIN_ROOM', ({ cid }) => {
+      socket.join(`${cid}`);
+    });
+    socket.on('CLIENT:SEND_MSG', async ({ cid, content, type, senderId }) => {
+      const msg = await userUseCases.sendMsg(cid, content, type, senderId);
+      socket.to(`${cid}`).emit('SERVER:SEND_MSG', msg);
+      socket.emit('SERVER:SAVED_MSG', msg);
+    });
+  });
+
   server.listen(PORT, () => console.log(`READY IN PORT [${PORT}]`));
 }
 
