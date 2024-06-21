0d46203e72500032c6a1837432f7b71c88557efa
Andres <lopez.andresrigoberto@gmail.com>
2024-06-21 08:06:40 -0700

    edit socket io file

--- src/infrastructure/socketIO.ts
+++ src/infrastructure/socketIO.ts
@@ -1,11 +1,11 @@
 import http from 'http';
 import { Server as WebSocketServer } from 'socket.io';
-import UserUsesCases from '../application/userUseCase';
+import prismaClient from './config/prisma-client';
+import MessageUseCase from '../application/messageUseCase';
+import PostgresMessageRepository from './repository/postgresMessageRepository';
+import PostgresUserRepository from './repository/postgresUserRepository';
 
-export const initSocketIO = (
-  server: http.Server,
-  userUseCases: UserUsesCases
-) => {
+export const initSocketIO = (server: http.Server) => {
   const io = new WebSocketServer(server, {
     cors: {
       origin:
@@ -19,10 +19,24 @@ export const initSocketIO = (
     socket.on('CLIENT:JOIN_ROOM', ({ cid }) => {
       socket.join(`${cid}`);
     });
-    socket.on('CLIENT:SEND_MSG', async ({ cid, content, type, senderId }) => {
-      const msg = await userUseCases.sendMsg(cid, content, type, senderId);
-      socket.to(`${cid}`).emit('SERVER:SEND_MSG', msg);
-      socket.emit('SERVER:SAVED_MSG', msg);
+    socket.on('CLIENT:SEND_MSG', async (message) => {
+      const { cid } = message;
+
+      const prisma = prismaClient.getInstance();
+
+      const postgresMessageRepository =
+        PostgresMessageRepository.getInstance(prisma);
+      const postgresUserRepository = PostgresUserRepository.getInstance(prisma);
+
+      const messageUseCase = MessageUseCase.getInstance(
+        postgresMessageRepository,
+        postgresUserRepository
+      );
+
+      socket
+        .to(`${cid}`)
+        .emit('SERVER:SEND_MSG', messageUseCase.sendMessage(message));
+      socket.emit('SERVER:SAVED_MSG', message);
     });
   });
 };
