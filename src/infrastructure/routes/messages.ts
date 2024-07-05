5c01f161d4277aa101daee047701bb9607a6a08a
Andres <lopez.andresrigoberto@gmail.com>
2024-07-04 21:14:11 -0700

    remove user repository from dependencies

--- src/infrastructure/routes/messages.ts
+++ src/infrastructure/routes/messages.ts
@@ -3,18 +3,14 @@ import prismaClient from '../config/prisma-client';
 import MessageUseCase from '../../application/messageUseCase';
 import PostgresMessageRepository from '../repository/postgresMessageRepository';
 import MessagesController from '../controller/message';
-import PostgresUserRepository from '../repository/postgresUserRepository';
 // import { io } from '../../server';
 
 export const messagesRouter = Router();
 
 const prisma = prismaClient.getInstance();
 const messageRepository = PostgresMessageRepository.getInstance(prisma);
-const userRepository = PostgresUserRepository.getInstance(prisma);
-const messageUseCases = MessageUseCase.getInstance(
-  messageRepository,
-  userRepository
-);
+
+const messageUseCases = MessageUseCase.getInstance(messageRepository);
 
 const messagesController = new MessagesController(messageUseCases);
 
