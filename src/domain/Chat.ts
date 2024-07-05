1515e10d125f94bae7df423f88d8c961c692853e
Andres <lopez.andresrigoberto@gmail.com>
2024-07-04 21:05:22 -0700

    delete unused types

--- src/domain/Chat.ts
+++ src/domain/Chat.ts
@@ -1,21 +1,7 @@
-import { UserEntity } from './User';
-import { MsgEntity } from './Message';
-
-export interface Chat {
+export interface ChatEntity {
   cid: string;
   alias: string | null;
   creationDate: Date;
   chatImage: string | null;
   isGroup: boolean;
-}
-
-export interface ChatEntity {
-  cid: string;
-  alias?: string;
-  creationDate: Date;
-  chatImage?: string;
-  isGroup: boolean;
-  admins: UserEntity[];
-  members: UserEntity[];
-  messages: MsgEntity[];
 }
